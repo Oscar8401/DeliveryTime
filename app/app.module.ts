38eb2a777b4bf8e55dca3dbd75e8952dd582d14c
Taleb01 <taleb_n1@hotmail.com>
2019-09-01 21:37:12 +0200

    import button model

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @NgModule({
-  imports: [ BrowserModule, BrowserAnimationsModule, GridModule ],
+  imports: [ BrowserModule, BrowserAnimationsModule, GridModule, ButtonsModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
