7aabfa035975511c384ffccd9ad62ce184267001
Taleb01 <taleb_n1@hotmail.com>
2019-09-01 09:18:55 +0200

    add sort function

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { deliveries } from './delivery';
+import { SortDescriptor, orderBy } from '@progress/kendo-data-query';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+
 
 @Component({
     selector: 'my-app',
@@ -7,5 +10,24 @@ import { deliveries } from './delivery';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public gridView: GridDataResult;
+  public sort: SortDescriptor [] = [{
+    field: 'startTime',
+    dir: 'asc'
+  }];
     public slots: any[] = deliveries;
+    constructor() {
+        this.loadDeliveries();
+    }
+        public sortChange(sort: SortDescriptor[]): void {
+        this.sort = sort;
+        this.loadDeliveries();
+    }
+
+    private loadDeliveries(): void {
+        this.gridView = {
+            data: orderBy(this.slots, this.sort),
+            total: this.slots.length
+        };
+    }
 }
